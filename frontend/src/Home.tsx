81728ca0ad956d65f4fe8341b3b6313dfa0a08e5
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2024-02-07 07:44:58 -0700

    style(homepage): layout adjustments + coloring

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,13 +1,28 @@
 import 'react';
 import { Link } from 'react-router-dom';
+import { Nav } from './components/Nav';
 
 export const Home = () => {
   return (
-    <>
-      <h1>Goalboard</h1>
+    <div className="min-h-screen">
+      <Nav />
+      <main className="pt-20 text-center">
+        <h1 className="text-4xl font-bold mb-20">Goalboard</h1>
 
-      <Link to="/login">Sign in</Link>
-      <Link to="/register">Sign up</Link>
-    </>
+        <section>
+          <Link to="/login">
+            <span className="rounded-2xl bg-cyan-600 text-white inline-block w-20 py-2">
+              Sign in
+            </span>
+          </Link>
+          <br className="mb-6" />
+          <Link to="/register">
+            <span className="rounded-2xl border border-cyan-600 text-cyan-600 inline-block w-20 py-2">
+              Sign up
+            </span>
+          </Link>
+        </section>
+      </main>
+    </div>
   );
 };

5e390be73c8b596c2344c47390e1a42dbfb92675
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2024-02-08 09:30:28 -0700

    refactor(color): use primary color instead of unset

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export const Nav = () => {
   return (
-    <nav className="bg-cyan-600 p-4 flex justify-between align-middle">
+    <nav className="bg-primary p-4 flex justify-between align-middle">
       <section className="flex align-middle">
         <Link to="/">
           <span className="text-lg font-bold text-white">Goalboard</span>

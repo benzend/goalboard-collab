6fbbc6676b63e8c6b669de45242bebacdcc79f86
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2024-02-21 07:35:44 -0700

    refactor(env): move ReadEnv to own file
    to organize a smidgeon

--- backend/database/psql.go
+++ backend/database/psql.go
@@ -1,50 +1,20 @@
 package database
 
 import (
-	"bufio"
 	"database/sql"
 	"fmt"
-	"os"
-	"strings"
 
+	"github.com/benzend/goalboard/backend/env"
 	_ "github.com/lib/pq"
 )
 
-func ReadEnvFile(filePath string) (map[string]string, error) {
-	env := make(map[string]string)
 
-	// Open the .env file
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, fmt.Errorf("error opening .env file: %v", err)
-	}
-	defer file.Close()
-
-	// Read lines from the file and parse key-value pairs
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		parts := strings.SplitN(line, "=", 2)
-
-		if len(parts) == 2 {
-			key := strings.TrimSpace(parts[0])
-			value := strings.TrimSpace(parts[1])
-			env[key] = value
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		return nil, fmt.Errorf("error reading .env file: %v", err)
-	}
-
-	return env, nil
-}
 
 func Connect() (db *sql.DB, err error) {
 
 	//Read in .env file keys
 
-	env, err := ReadEnvFile(".env")
+	env, err := env.ReadFile(".env")
 
 	host := env["host"]
 	password := env["password"]

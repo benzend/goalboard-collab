1023190910e7285541faabd018b0462e8d3c2416
Benjamin <benjamin.scottt.dev@gmail.com>
2024-02-26 11:06:26 -0700

    chore: hookup ctx, db, register, login, etc (#28)

--- frontend/src/utils/user.ts
+++ frontend/src/utils/user.ts
@@ -19,7 +19,7 @@ export type CreateUserData = {
 };
 
 export async function createUser(data: CreateUserData): Promise<User> {
-  const res = await fetch('http://localhost:8000/CreateUser', {
+  const res = await fetch('http://localhost:8000/register', {
     body: JSON.stringify(data),
     method: 'POST',
   });
@@ -34,7 +34,14 @@ export type LoginUserData = {
   password: string;
 };
 
-export async function loginUser(data: LoginUserData): Promise<User> {
+export type LoginUserReturnData = {
+  token: string;
+  user: User;
+};
+
+export async function loginUser(
+  data: LoginUserData
+): Promise<LoginUserReturnData> {
   const res = await fetch('http://localhost:8000/login', {
     body: JSON.stringify(data),
     method: 'POST',

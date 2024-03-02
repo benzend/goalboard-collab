f4ca95e9384d10612c102ee9c77133c42ab84b7a
Alex Gwartney <gwartneydev@gmail.com>
2024-03-02 04:44:42 -0800

    fixing migrations

--- backend/database/migrations/000001_creategoals_table.up.sql
+++ backend/database/migrations/000001_creategoals_table.up.sql
@@ -1,11 +1,14 @@
 BEGIN;
 
- 
 CREATE TABLE IF NOT EXISTS goals_ (
-	goalId          SERIAL PRIMARY KEY,
-    Name            VARCHAR(50)
-    TargetPerDay    VARCHAR     
-    LongTermTarget  VARCHAR     
+    goalId          SERIAL PRIMARY KEY,
+    Name            VARCHAR(50),
+    TargetPerDay    VARCHAR,
+    LongTermTarget  VARCHAR,
+    user_id         INT,  -- Define the user_id column
+    CONSTRAINT user_fk
+        FOREIGN KEY(user_id) 
+        REFERENCES user_(id)  -- Reference the id column in the user_ table
 );
 
 COMMIT;

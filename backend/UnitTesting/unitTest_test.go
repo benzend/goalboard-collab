69e7a5fec3d22f8b7dc79843259040e82851cf01
Gwartneydev <gwartneydev@gmail.com>
2024-04-11 01:30:16 -0700

    updated unit testing

--- backend/UnitTesting/unitTest_test.go
+++ backend/UnitTesting/unitTest_test.go
@@ -3,43 +3,60 @@ package UnitTesting
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/benzend/goalboard/pw"
+	"github.com/benzend/goalboard/router"
 	"github.com/benzend/goalboard/routes"
-	// Import Testify/assert package
+	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateActivity(t *testing.T) {
-	// Mock request body
-	requestBody := []byte(`{"progress": "some_progress_value"}`)
-	// Create a mock request with the request body
-	req, err := http.NewRequest("POST", "/activities", bytes.NewBuffer(requestBody))
-	if err != nil {
-		t.Fatal(err)
-	}
-	// Create a mock response recorder
+type MockDb struct{}
+
+func TestCreateUser(t *testing.T) {
+	// Create a new mock SQL database connection.
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	ctx := context.Background()
+	ctxWithValueDB := context.WithValue(ctx, "db", db)
+	// Define the test user credentials.
+	username := "testuser"
+	password := "testpass"
+
+	hashedPassword, err := pw.HashPassword(password)
+	assert.NoError(t, err)
+
+	// Set up the expectation for the INSERT statement.
+	mock.ExpectExec("INSERT INTO user_").
+		WithArgs(username, hashedPassword).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	// Create a request body with user credentials.
+	reqBody := []byte(fmt.Sprintf(`{"username": "%s", "password": "%s"}`, username, password))
+
+	// Create a new HTTP request with the request body.
+	req, err := http.NewRequest("POST", "/register", bytes.NewBuffer(reqBody))
+	assert.NoError(t, err)
+
+	// Create a mock response recorder.
 	rr := httptest.NewRecorder()
 
-	var ctx = context.Background()
-	// Hello world, the web server
+	// Call the Register handler function with the request and recorder.
+	routes.Register(ctxWithValueDB, rr, req)
 
-	if err != nil {
-		panic(err)
+	router := router.NewRouter()
+	router.Ctx(ctxWithValueDB)
+
+	router.Post("/register", routes.Register)
+
+	// Check the response status code.
+	if rr.Code != http.StatusInternalServerError {
+		t.Errorf("expected status code %d but got %d", http.StatusInternalServerError, rr.Code)
 	}
 
-	// Call the CreateActivity handler function
-	routes.CreateActivity(ctx, rr, req)
-
-	// Check the response status code
-	if rr.Code != http.StatusCreated {
-		t.Errorf("expected status code %d but got %d", http.StatusCreated, rr.Code)
-	}
-
-	// Check the response body
-	expectedResponse := "Goal data inserted successfully\n"
-	if rr.Body.String() != expectedResponse {
-		t.Errorf("expected response body %q but got %q", expectedResponse, rr.Body.String())
-	}
 }

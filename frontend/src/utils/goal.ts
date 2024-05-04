128288a798a326c551fa7c9d8a9a3f2e50c1aa82
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2024-05-04 14:04:12 -0600

    fix(goals): restructure response obj

--- frontend/src/utils/goal.ts
+++ frontend/src/utils/goal.ts
@@ -6,7 +6,7 @@ export type Goal = {
   durPer: string;
 };
 
-export async function getGoals(): Promise<Goal[]> {
+export async function getGoals(): Promise<{ goals: Goal[] }> {
   const res = await fetch(`http://localhost:8000/goals`);
 
   if (!res.ok) throw new Error('failed to fetch goals');
